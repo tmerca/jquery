cef3e405e6870d6ed2039bd41672b965c5830de6
andreu mercadal <amercadal20233@iesjoanramis.org>
2024-01-11 21:16:11 +0100

    memory

--- act2/index.js
+++ act2/index.js
@@ -3,30 +3,58 @@ $(document).ready(function () {
 });
 
 function createGame() {
-  var numOfDivs = Math.floor(Math.random() * 10) + 2;
+  var arr1 = [];
+  var arr2 = [];
   var content = [];
+  var couple = false;
+  var numOfCouples = (Math.floor(Math.random() * 4) + 2) * 2;
+
+  if (numOfCouples % 2 != 0) {
+    numOfCouples += 1;
+  }
+
+  for(let i = 1; i < numOfCouples; i++){
+    arr1[i - 1] = i;
+    arr2[i - 1] = i;
+  }
+  content = [...arr1,...arr2];
+  
+  // Unsorting the content of the table
+  // function shuffleArray(content) {
+  //   content.sort(() => Math.random() - 0.5);
+  // }
+  // shuffleArray(content);
+  console.log(content);
 
   $("body").append("<ul class='table'></ul>");
 
-  if (numOfDivs % 2 != 0) {
-    numOfDivs += 1;
-  }
+  // Creating two boxes with the same number and adding some css
 
-  // Creating to boxes with the same number and adding some css
-  for (i = 1; i <= numOfDivs; i++) {
+  for(let i = 0; i < content.length; i++){
+    $('.table').append("<li><div class='box'><p>"+ content[i] +"</p></div></li>");
   }
-
+   
   $("li").css("display", "inline-block");
-
   $(".box").css("width", "100px").css("height", "100px");
   $(".box").css("background-color", "lightgrey").css("margin", "10px 10px");
   $(".box").css("border", "2px black solid");
   $(".box").css("border-radius", "4px");
   $(".box").css("display", "flex");
   $(".box").css("justify-content", "center").css("align-items", "center");
-
-  // Sorting the content of the table
+  
+  $("p").hide();
+  
+  let numClicks = 0;
+  // Taking the value of the box we click
+  if(numClicks < 2){
+    $(".box").on("click", function() {
+      
+      $(this).find("p").show();
+      numClicks+=1;
+    });
+  }else {
+    $("p").hide();
+  }
   
 
-  // Taking the value of the box we click
 }
